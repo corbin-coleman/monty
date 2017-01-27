60ec4445fdc58206e6786c1e29bdec620f05e848
Corbin <corbin.coleman@holbertonschool.com>
2017-01-27 02:55:44 +0000

    error check other getline

--- main.c
+++ main.c
@@ -47,6 +47,11 @@ int main(int argc, char *argv[])
 		}
 		line_num++;
 		read = getline(&line, &size, file);
+		if (read == -1 && line == NULL)
+		{
+			printf("Error: Malloc failed\n");
+			free_and_exit(line, file, stack);
+		}
 	}
 	free_stack(stack);
 	free(line);
